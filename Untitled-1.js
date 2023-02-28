624cc9f309ae689d89b981ae1ee39b97f8a6c728
Wesley Tran <122310175+wesleytran77@users.noreply.github.com>
2023-02-27 20:27:57 -0600

    Update Untitled-1.js

--- Untitled-1.js
+++ Untitled-1.js
@@ -3,6 +3,6 @@ const callButton = document.querySelectorAll(".phone")
 
 callButton.forEach(button =>{
     button.addEventListener("click", function(){
-        window.location.href = "tel:+9562619528";
+        window.location.href = "tel:+1 9562619528";
     })
 })
